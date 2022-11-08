fc59f6cce3bff0ccd29762a32df18e931b39d443
Ignatov Alexander <ignatov.a.alexander@gmail.com>
2022-11-08 20:46:09 +0700

    создание игры крестики/нолики в ТГ

--- HomeWork_Lession9/tg_bot.py
+++ HomeWork_Lession9/tg_bot.py
@@ -1,31 +1,100 @@
 import telebot
-from random import *
-import json
 from telebot import types
-import XO
-films=[]
-
-
-def save():
-    with open("films.json","w",encoding="utf-8") as fh:
-        fh.write(json.dumps(films,ensure_ascii=False))
-    print("Наша фильмотека была успешно сохранена в файле films.json")
-
-def load():
-    global films
-    with open("films.json","r",encoding="utf-8") as fh:
-        films=json.load(fh)
-    print("Фильмотека была успешно загружена")   
 
 API_TOKEN='5323012092:AAFI4ggChHGk0wvgn9ix6UbfQNbxsYw0Ylw'
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    # try:
-    XO.startGame(message)
-    bot.send_message(message.chat.id,"It's alive!")
-    # except:
-    #     bot.send_message(message.chat.id,"Что-то пошло не так")
+    startGame(message)
+
+@bot.message_handler(content_types='text')
+def message_reply(mes):
+    send = bot.send_message(mes.chat.id,'Следующий ход')
+    bot.register_next_step_handler(send, game)
+
+def startGame(message):
+    step = int(firstStep(message))
+    game(step, message)
+
+
+def firstStep(message):
+    from random import randint
+
+    if randint(1, 3) == 1:
+        bot.send_message(message.chat.id,"Первым ходит игрок1")
+        step = 1
+        b = randint(1, 29)
+    else:
+        step = 2
+        bot.send_message(message.chat.id,"Первым ходит игрок2")
+    return step
+
+def game (player, message):
+    amount = 0
+    import re
+    pattern='\s+'
+    newM = newMatrix()
+    
+    while amount < 9:
+        if player == 1:
+            bot.send_message(message.chat.id,"Игорк 1. Введите номер строки и столбца через пробел  ")
+            str_lst= str(message_reply())
+            str_lst = re.split(pattern, str_lst)
+            newM[int(str_lst[0])][int(str_lst[1])] = 'O'
+            amount += 1
+            player += 1
+            printMatrix(newM)
+            if checkWin (newM):
+                bot.send_message(message.chat.id,"Победа за Игроком 1")
+                # print("Победа за Игроком 1")
+                quit()
+
+        else:
+            bot.send_message(message.chat.id,"Игорк 2. Введите номер строки и столбца через пробел  ")
+            str_lst= str(message_reply())
+            str_lst = re.split(pattern,str_lst)
+            newM[int(str_lst[0])][int(str_lst[1])] = 'X'
+            amount += 1
+            player -= 1
+            printMatrix(newM)
+            if checkWin (newM):
+                bot.send_message(message.chat.id,"Победа за Игроком 2")
+                # print("Победа за Игроком 2")
+                quit()
+
+def newMatrix():
+    matrix = []
+    for i in range(3):
+        sp = []
+        for j in range(3):
+            sp.append(".")
+        matrix.append(sp)
+    return matrix
+
+def printMatrix(matrix):
+    for i in range(3):
+        print(" ".join(matrix[i]))
+
+def checkWin(matrix):
+    result = False
+    a = 'X'
+    b = 'O'
+    for i in range(3):
+        if matrix[i][0]==matrix[i][1]==matrix[i][2]==a:
+            result = True
+        elif matrix[i][0]==matrix[i][1]==matrix[i][2]==b:
+            result = True
+    for i in range(3):
+        if matrix[0][i]==matrix[1][i]==matrix[2][i]==a:
+            result = True
+        elif matrix[0][i]==matrix[1][i]==matrix[2][i]==b:
+            result = True
+    if matrix[0][0]==matrix[1][1]==matrix[2][2]==a:
+            result = True
+    elif matrix[0][0]==matrix[1][1]==matrix[2][2]==b:
+        result = True
+    return result
+
 
 bot.polling()

790c59f6c4a2ba725edc0c1e7c3a4fb4728a6bd9
Ignatov Alexander <ignatov.a.alexander@gmail.com>
2022-11-10 22:28:26 +0700

    Телеграм бот для игры в крестики/нолики

--- HomeWork_Lession9/tg_bot.py
+++ HomeWork_Lession9/tg_bot.py
@@ -1,103 +1,131 @@
-# import telebot
-# from telebot import types
+import telebot
+from telebot import types
+import json
 
-# API_TOKEN='5323012092:AAFI4ggChHGk0wvgn9ix6UbfQNbxsYw0Ylw'
-# bot = telebot.TeleBot(API_TOKEN)
+API_TOKEN='5323012092:AAFI4ggChHGk0wvgn9ix6UbfQNbxsYw0Ylw'
+bot = telebot.TeleBot(API_TOKEN)
  
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     startGame(message)
-
-# @bot.message_handler(content_types='text')
-# def message_reply(mes):
-#     send = bot.send_message(mes.chat.id,'Следующий ход')
-#     bot.register_next_step_handler(send, game)
-
-# def startGame(message):
-#     step = int(firstStep(message))
-#     game(step, message)
-
-# def firstStep(message):
-#     from random import randint
-
-#     if randint(1, 3) == 1:
-#         bot.send_message(message.chat.id,"Первым ходит игрок1")
-#         step = 1
-#         b = randint(1, 29)
-#     else:
-#         step = 2
-#         bot.send_message(message.chat.id,"Первым ходит игрок2")
-#     return step
-
-# def game (player, message):
-#     amount = 0
-#     import re
-#     pattern='\s+'
-#     newM = newMatrix()
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup0 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Бросьте монетку")
+    markup0.add(btn1)
+    bot.send_message(message.chat.id,text="Игра запущена, бростье монетку",reply_markup=markup0)
+    markup0 = telebot.types.ReplyKeyboardRemove()
     
-#     while amount < 9:
-#         if player == 1:
-#             bot.send_message(message.chat.id,"Игорк 1. Введите номер строки и столбца через пробел  ")
-#             @bot.message_handler(content_types='text')
-#             def message_reply(mes):
-#                 send = bot.send_message(mes.chat.id,'Следующий ход')
-#                 bot.register_next_step_handler(send, game)
-#             str_lst=message_reply(mes)
-#             str_lst = re.split(pattern, str_lst)
-#             newM[int(str_lst[0])][int(str_lst[1])] = 'O'
-#             amount += 1
-#             player += 1
-#             printMatrix(newM)
-#             if checkWin (newM):
-#                 bot.send_message(message.chat.id,"Победа за Игроком 1")
-#                 # print("Победа за Игроком 1")
-#                 quit()
+@bot.message_handler(content_types=['text'])
+def frst(message):
+    if(message.text == "Бросьте монетку"):
+        from random import randint
+        a = telebot.types.ReplyKeyboardRemove()
+        if randint(1, 3) == 1:
+            bot.send_message(message.chat.id,text="Первым ходит игрок 1. Поставьте X", reply_markup=boardCreate())
+            bot.register_next_step_handler(message, game1)
+        else:
+            bot.send_message(message.chat.id,text="Первым ходит игрок 2. Поставьте O", reply_markup=boardCreate())
+            bot.register_next_step_handler(message, game2)
+    else:
+        bot.send_message(message.chat.id,"Ну что ж вы так!")
+ 
+@bot.message_handler(content_types=['text'])
+def game1(message):
+    a = 'X'
+    if checkWin ():
+        bot.send_message(message.chat.id,text="Победа за Игроком 2. Игра завершена")
+        quit()
+    else:
+        bot.send_message(message.chat.id,text="Ход игрока 2. Поставьте O",reply_markup=boardChange(message, a))
+        bot.register_next_step_handler(message, game2)
 
-#         else:
-#             bot.send_message(message.chat.id,"Игорк 2. Введите номер строки и столбца через пробел  ")
-#             message_reply()
-#             str_lst = re.split(pattern,str_lst)
-#             newM[int(str_lst[0])][int(str_lst[1])] = 'X'
-#             amount += 1
-#             player -= 1
-#             printMatrix(newM)
-#             if checkWin (newM):
-#                 bot.send_message(message.chat.id,"Победа за Игроком 2")
-#                 # print("Победа за Игроком 2")
-#                 quit()
+@bot.message_handler(content_types=['text'])
+def game2(message):
+    a = 'O'
+    if checkWin ():
+        bot.send_message(message.chat.id,text="Победа за Игроком 1. Игра завершена")
+        quit()
+    else:
+        bot.send_message(message.chat.id,text="Ход игрока 1.Поставьте X",reply_markup=boardChange(message, a))
+        bot.register_next_step_handler(message, game1)
 
-# def newMatrix():
-#     matrix = []
-#     for i in range(3):
-#         sp = []
-#         for j in range(3):
-#             sp.append(".")
-#         matrix.append(sp)
-#     return matrix
+def boardCreate():
+    XO_BDinit = XO_board()
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    row = [types.KeyboardButton(x) for x in XO_BDinit[:3]]
+    markup.add(*row)
+    row = [types.KeyboardButton(x) for x in XO_BDinit[3:6]]
+    markup.add(*row)
+    row = [types.KeyboardButton(x) for x in XO_BDinit[6:]]
+    markup.add(*row)
+    return markup
 
-# def printMatrix(matrix):
-#     for i in range(3):
-#         print(" ".join(matrix[i]))
+def boardCreate2():
+    global XO_BD_changed
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    row = [types.KeyboardButton(x) for x in XO_BD_changed[:3]]
+    markup.add(*row)
+    row = [types.KeyboardButton(x) for x in XO_BD_changed[3:6]]
+    markup.add(*row)
+    row = [types.KeyboardButton(x) for x in XO_BD_changed[6:]]
+    markup.add(*row)
+    return markup
 
-# def checkWin(matrix):
-#     result = False
-#     a = 'X'
-#     b = 'O'
-#     for i in range(3):
-#         if matrix[i][0]==matrix[i][1]==matrix[i][2]==a:
-#             result = True
-#         elif matrix[i][0]==matrix[i][1]==matrix[i][2]==b:
-#             result = True
-#     for i in range(3):
-#         if matrix[0][i]==matrix[1][i]==matrix[2][i]==a:
-#             result = True
-#         elif matrix[0][i]==matrix[1][i]==matrix[2][i]==b:
-#             result = True
-#     if matrix[0][0]==matrix[1][1]==matrix[2][2]==a:
-#             result = True
-#     elif matrix[0][0]==matrix[1][1]==matrix[2][2]==b:
-#         result = True
-#     return result
+def XO_board():
+    XO_BD_init = ["1","2","3","4","5","6","7","8","9"]
+    return XO_BD_init
 
+def boardChange(message,a):
+    global XO_BD
+    XO_BD = XO_board()
+    global XO_BD_changed
+    for i in range(0,9):
+        if XO_BD[i] == message.text:
+            XO_BD_changed [i] = a
+    return boardCreate2()
 
-# bot.polling()
+def checkWin():
+    result = False
+    global XO_BD_changed
+    matrix = XO_BD_changed
+    a = 'X'
+    b = 'O'
+    i = 0
+    if matrix[i]==matrix[i+1]==matrix[i+2]==a:
+        result = True
+    elif matrix[i]==matrix[i+1]==matrix[i+2]==b:
+            result = True
+    i = 3
+    if matrix[i]==matrix[i+1]==matrix[i+2]==a:
+        result = True
+    elif matrix[i]==matrix[i+1]==matrix[i+2]==b:
+            result = True
+    i = 6
+    if matrix[i]==matrix[i+1]==matrix[i+2]==a:
+        result = True
+    elif matrix[i]==matrix[i+1]==matrix[i+2]==b:
+            result = True
+    if matrix[0]==matrix[4]==matrix[8]==a:
+            result = True
+    elif matrix[0]==matrix[4]==matrix[8]==b:
+        result = True
+    if matrix[2]==matrix[4]==matrix[6]==a:
+            result = True
+    elif matrix[2]==matrix[4]==matrix[6]==b:
+        result = True
+    if matrix[0]==matrix[3]==matrix[6]==a:
+            result = True
+    elif matrix[0]==matrix[3]==matrix[6]==b:
+        result = True 
+    if matrix[1]==matrix[4]==matrix[7]==a:
+            result = True
+    elif matrix[1]==matrix[4]==matrix[7]==b:
+        result = True  
+    if matrix[2]==matrix[5]==matrix[8]==a:
+            result = True
+    elif matrix[2]==matrix[5]==matrix[8]==b:
+        result = True
+    return result
+
+XO_BD_changed = ["1","2","3","4","5","6","7","8","9"]
+
+bot.polling()
+
